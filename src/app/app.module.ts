27050fccef11be8f0d8bcda951862763b44275a4
Ricardo Molina <toorop.rmc@gmail.com>
2020-04-10 19:38:43 +0200

    Updated to angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule, JsonpModule } from '@angular/http';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 //servicios
@@ -18,8 +18,8 @@ import { SearchComponent } from './components/search/search.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    JsonpModule
+    HttpClientModule,
+    HttpClientJsonpModule
   ],
   providers: [
     FilmsService
